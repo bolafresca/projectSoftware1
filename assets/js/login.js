8ae36a7f3a821e7630dab16cb9ffa0b7bfa24a26
bolafresca <106411372+bolafresca@users.noreply.github.com>
2022-06-24 17:59:56 -0500

    Pagina funcionando 1.2

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,6 @@
 function Redirect() {
     let itsOk;
+    let itsOKadmin;
     let user = document.getElementById('user').value;
     let password = document.getElementById('password').value;
 
@@ -12,4 +13,25 @@ function Redirect() {
     if (itsOk==true) {
         location.href = "student-menu.html";
     }
+    //segundo
+    if (user == "admin" && password == "12345") {
+        itsOkadmin = true;
+    } else {
+        itsOkadmin = false;
+    }
+
+    if (itsOkadmin=true) {
+        location.href = "admin-menu.html";
+    }
+    //tercero
+    // if (user == "estudiante" && password == "1234") {
+    //     itsOk = true;
+    // } else {
+    //     itsOk = false;
+    // }
+
+    // if (itsOk==true) {
+    //     location.href = "student-menu.html";
+    // }
+    
 }
